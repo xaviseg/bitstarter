4b6a5f1689500ab6d7c78b3dc7238cfa3e3c714b
Xavier Segura <xaviseg@gmail.com>
2013-07-10 21:33:57 +0000

    read from file5

--- web.js
+++ web.js
@@ -6,7 +6,6 @@ var fs = require('fs');
 var fstring = fs.readFileSync('index.html',{encoding: 'utf8'}); 
 
 app.get('/', function(request, response) {
-//  response.send('Hello World 2!');
   response.send(fstring);
 });
 
