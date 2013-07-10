84621c3f407936040e9a76b8fca468d6fbe894f6
Xavier Segura <xaviseg@gmail.com>
2013-07-10 21:21:27 +0000

    read from file2

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = requiere('fs');
-var fstring = fs.readFileSync(index.html,{encoding: utf-8} 
+var fstring = fs.readFileSync(index.html,{encoding: utf-8}); 
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
